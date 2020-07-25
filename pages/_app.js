f5d91e79a9a2558db6e38854a8d69568091d5918
yurii-oliiarnyk <oliiarnyk14@gmail.com>
2020-07-25 19:32:24 +0300

    Added app progressbar

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,14 @@
+import NextNprogress from "nextjs-progressbar";
+
 export default function MyApp({ Component, pageProps }) {
   return (
     <>
+      <NextNprogress
+        color="#29D"
+        startPosition={0.3}
+        stopDelayMs={200}
+        height="3"
+      />
       <Component {...pageProps} />
       <style jsx global>{`
         html {

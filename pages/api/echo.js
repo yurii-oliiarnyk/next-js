f6eb7123cc81e354339422fa0d2e5cf6bf130185
yurii-oliiarnyk <oliiarnyk14@gmail.com>
2020-07-25 19:42:48 +0300

    Create simple api point

---
+++ pages/api/echo.js
@@ -0,0 +1,13 @@
+export default function echo(req, res) {
+  // res.statusCode = 200;
+  // res.setHeader("Content-Type", "application/json");
+  // res.end(
+  //   JSON.stringify({
+  //     message: req.query.message ?? "Base message",
+  //   })
+  // );
+
+  res.json({
+    message: req.query.message ?? "Base message",
+  });
+}
